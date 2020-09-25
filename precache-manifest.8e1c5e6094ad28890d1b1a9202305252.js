468009dc56ac3f92d687b76c9c497f1cd4980de5
Rohini210 <prohini210@gmail.com>
2020-09-25 16:08:58 +0530

    Updates

---
+++ precache-manifest.8e1c5e6094ad28890d1b1a9202305252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d70e2c8cd378f46bb579feed3b9f98",
+    "url": "/News_API/index.html"
+  },
+  {
+    "revision": "6fff72c68d473224b93e",
+    "url": "/News_API/static/css/main.6f085ee9.chunk.css"
+  },
+  {
+    "revision": "8f1be64ae4f5adc73392",
+    "url": "/News_API/static/js/2.d96db309.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/News_API/static/js/2.d96db309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fff72c68d473224b93e",
+    "url": "/News_API/static/js/main.9bba3389.chunk.js"
+  },
+  {
+    "revision": "c7fa33f1eb22e64f5027",
+    "url": "/News_API/static/js/runtime-main.c2b7cb7e.js"
+  }
+]);
